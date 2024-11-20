50b20966bb791be5f0f88a09950e9b2ba739562a
marcos.goncalves <kirottow@gmail.com>
2024-11-20 17:29:03 -0300

    refactor: Adicionar tratamento de erros na rota principal do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import fastify from "fastify";
 import taskRoutes from "./routes/TaskRoutes";
+import { registerErrorHandler } from "./errors/ErrorHandler";
 
 const app = fastify();
 const PORT = process.env.PORT || 3333;
 
 app.register(taskRoutes);
+
+// Register error handler
+registerErrorHandler(app);
 
 // Start the server
 app.listen({
